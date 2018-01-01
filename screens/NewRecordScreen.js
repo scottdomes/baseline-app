8e7fa02512b8a144474881d985b1030f75d3b846
YScott Domes <scott@musefind.com>
2017-12-31 16:14:24 -0800

    Added button selection

--- screens/NewRecordScreen.js
+++ screens/NewRecordScreen.js
@@ -4,17 +4,42 @@ import { ExpoLinksView } from "@expo/samples";
 import { Button } from "react-native-elements";
 
 export default class LinksScreen extends React.Component {
+  state = { selectedNumber: null };
   static navigationOptions = {
     title: "New"
   };
+
+  handleSelectNumber(selectedNumber) {
+    if (
+      this.state.selectedNumber &&
+      this.state.selectedNumber === selectedNumber
+    ) {
+      this.setState({ selectedNumber: null });
+    } else {
+      this.setState({ selectedNumber });
+    }
+  }
 
   render() {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
       <ScrollView style={styles.container}>
-        {arr.map(num => {
-          return <Button style={styles.button} key={num} large title={`${num}`} />;
-        })}
+        {arr
+          .filter(
+            num =>
+              !this.state.selectedNumber || this.state.selectedNumber === num
+          )
+          .map(num => {
+            return (
+              <Button
+                onPress={this.handleSelectNumber.bind(this, num)}
+                style={styles.button}
+                key={num}
+                large
+                title={`${num}`}
+              />
+            );
+          })}
       </ScrollView>
     );
   }
