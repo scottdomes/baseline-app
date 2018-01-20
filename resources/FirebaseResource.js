e07bef29f00eeec9f9397b16258cfd537a62c695
YScott Domes <scott@musefind.com>
2018-01-20 10:06:04 -0800

    Started redux for setRecords

--- resources/FirebaseResource.js
+++ resources/FirebaseResource.js
@@ -3,8 +3,31 @@ import * as firebase from "firebase";
 class FirebaseResource {
   setListeners(login, logout, setRecords) {
     firebase.auth().onAuthStateChanged(user => {
-      login(user);
+      if (user) {
+        this.setRecordListener(setRecords, user.uid);
+        login(user);
+      } else {
+        logout();
+      }
     });
+  }
+
+  setRecordListener(setRecords, userId) {
+    firebase
+      .database()
+      .ref(`${userId}/records/`)
+      .on("value", snapshot => {
+        if (snapshot.val() === null) {
+          setRecords([]);
+        } else {
+          const records = Object.keys(snapshot.val()).map(key => {
+            const record = snapshot.val()[key];
+            record.id = key;
+            return record;
+          });
+          setRecords(records);
+        }
+      });
   }
 }
 
