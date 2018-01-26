a576e9c882ade5a1cd3355223b5fa6c25b052e89
YScott Domes <scott@musefind.com>
2018-01-25 17:53:13 -0800

    Added tag display

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,6 +16,7 @@ import * as firebase from "firebase";
 import { MonoText } from "../components/StyledText";
 
 class HomeScreen extends React.Component {
+  state = { tagTree: {} };
   static navigationOptions = {
     header: null
   };
@@ -24,12 +25,43 @@ class HomeScreen extends React.Component {
     firebase.auth().signOut();
   };
 
+  componentDidMount() {
+    this.organizeTags(this.props.records, this.props.tags);
+  }
+
+  componentWillReceiveProps(next) {
+    if (
+      next.records.length !== this.props.records.length ||
+      next.tags.length !== this.props.tags.length
+    ) {
+      this.organizeTags(next.records, next.tags);
+    }
+  }
+
+  organizeTags = (records, tags) => {
+    const tree = {};
+    const tagNames = tags.map(tag => tag.name);
+    records.forEach(record => {
+      record.tags.forEach(tag => {
+        // If they have not deleted the tag
+        if (tagNames.indexOf(tag) > -1) {
+          if (tree[tag]) {
+            tree[tag] = (tree[tag] + record.value) / 2;
+          } else {
+            tree[tag] = record.value;
+          }
+        }
+      });
+    });
+    this.setState({ tagTree: tree });
+  };
+
   render() {
-    const { records } = this.props;
-    const values = records.map(record => record.value)
+    const { records, tags } = this.props;
+    const values = records.map(record => record.value);
     const avg =
       values.length > 0 &&
-      values.reduce((x, y) => x + y) / values.length;
+      (values.reduce((x, y) => x + y) / values.length).toFixed(1);
 
     const os = Platform.OS === "ios" ? "ios-" : "md-";
     return (
@@ -53,15 +85,24 @@ class HomeScreen extends React.Component {
             </Text>
           </View>
           <Button onPress={this.handleLogout} title="Logout" />
+          {Object.keys(this.state.tagTree).map(key => {
+            const val = this.state.tagTree[key];
+            return (
+              <Text key={key}>
+                {key} {val}
+              </Text>
+            );
+          })}
         </ScrollView>
       </View>
     );
   }
 }
 
-const mapStateToProps = ({ records }) => {
+const mapStateToProps = ({ records, tags }) => {
   return {
-    records
+    records,
+    tags
   };
 };
 
