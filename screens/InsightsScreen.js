54ca2c36c202fb449bbb74b83dfd7c1513ed05ae
YScott Domes <scott@musefind.com>
2018-01-27 01:25:47 -0800

    Added first chart

--- screens/InsightsScreen.js
+++ screens/InsightsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
@@ -8,18 +8,36 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
-import { ExpoConfigView } from '@expo/samples';
+import { ExpoConfigView } from "@expo/samples";
 import { connect } from "react-redux";
+import { VictoryTheme, VictoryChart, VictoryLine } from "victory-native";
+import moment from "moment";
 
 class InsightsScreen extends React.Component {
   static navigationOptions = {
-    title: 'Insights',
+    title: "Insights"
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <Text style={{marginTop: 100}}>{this.props.records.length}</Text>
+    console.log('render')
+    const { records } = this.props;
+    if (records.length === 0) {
+      return null;
+    }
+    return (
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine
+          style={{
+            data: { stroke: "#c43a31" },
+            parent: { border: "1px solid #ccc" }
+          }}
+          domain={{ y: [1, 10] }}
+          data={records.map((record, i) => {
+            return { y: record.value };
+          })}
+        />
+      </VictoryChart>
+    );
   }
 }
 
