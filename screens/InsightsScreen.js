2594aff288ead80725c88339bc52a5725f26b26c
YScott Domes <scott@musefind.com>
2018-01-27 01:01:15 -0800

    Connected insights screen

--- screens/InsightsScreen.js
+++ screens/InsightsScreen.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 import { ExpoConfigView } from '@expo/samples';
+import { connect } from "react-redux";
 
-export default class SettingsScreen extends React.Component {
+class InsightsScreen extends React.Component {
   static navigationOptions = {
     title: 'Insights',
   };
@@ -9,6 +19,15 @@ export default class SettingsScreen extends React.Component {
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return <Text style={{marginTop: 100}}>{this.props.records.length}</Text>
   }
 }
+
+const mapStateToProps = ({ records, tags }) => {
+  return {
+    records,
+    tags
+  };
+};
+
+export default connect(mapStateToProps, null)(InsightsScreen);
