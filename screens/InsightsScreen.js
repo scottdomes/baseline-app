3926cde81ebd305fb42358a5d17dba0a41920d7d
YScott Domes <scott@musefind.com>
2018-01-30 20:36:59 -0800

    Added tag colours

--- screens/InsightsScreen.js
+++ screens/InsightsScreen.js
@@ -14,22 +14,22 @@ import { Button } from "react-native-elements";
 import { VictoryTheme, VictoryChart, VictoryLine } from "victory-native";
 import moment from "moment";
 const COLORS = [
-  "#ffbd4b",
-  "#ffa350",
-  "#ff8a54",
-  "#ff7359",
-  "#ff5e5e",
-  "#ff637c",
-  "#ff6898",
-  "#ff6cb2",
-  "#ff71cb",
-  "#ff76e3",
-  "#ff7bf9",
-  "#f17fff",
-  "#de84ff"
+  "#f3a683",
+  "#3B3B98",
+  "#778beb",
+  "#e15f41",
+  "#c44569",
+  "#574b90",
+  "#f8a5c2",
+  "#63cdda",
+  "#596275",
+  "#B33771",
+  "#9AECDB",
+  "#58B19F",
+  "#82589F"
 ];
 class InsightsScreen extends React.Component {
-  state = { selectedTags: [] };
+  state = { selectedTags: [], tree: {} };
   static navigationOptions = {
     title: "Insights"
   };
@@ -44,10 +44,31 @@ class InsightsScreen extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.organizeRecords(this.props.records);
+  }
+
   componentWillReceiveProps(next) {
     if (next.navigation.state.params && next.navigation.state.params.name) {
       this.setState({ selectedTags: [next.navigation.state.params.name] });
     }
+    if (next.records.length !== this.props.records.length) {
+      this.organizeRecords(next.records);
+    }
+  }
+
+  organizeRecords(records) {
+    const tree = {};
+    records.forEach((record, i) => {
+      record.tags.forEach(tag => {
+        if (tree[tag]) {
+          tree[tag].records.push(record);
+        } else {
+          tree[tag] = { records: [] };
+        }
+      });
+    });
+    this.setState({ tree });
   }
 
   handleSelectAll() {
@@ -73,23 +94,47 @@ class InsightsScreen extends React.Component {
     if (records.length === 0) {
       return null;
     }
-    const data = records.filter(this.filterRecord);
     return (
       <ScrollView>
-        {data.length > 2 ? (
-          <VictoryChart
-            height={350}
-            theme={VictoryTheme.material}
-            scale={{ x: "time" }}
-          >
+        <VictoryChart
+          height={350}
+          theme={VictoryTheme.material}
+          scale={{ x: "time" }}
+        >
+          {this.state.selectedTags.length > 0 ? (
+            this.state.selectedTags.map((tag, i) => {
+              if (!this.state.tree[tag]) {
+                return null;
+              }
+              return (
+                <VictoryLine
+                  key={tag}
+                  style={{
+                    data: { stroke: COLORS[i] },
+                    parent: { border: "1px solid #ccc" }
+                  }}
+                  domain={{ y: [1, 10] }}
+                  data={this.state.tree[tag].records.map((record, i) => {
+                    return {
+                      y: record.value,
+                      x: new Date(record.timestamp)
+                    };
+                  })}
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 }
+                  }}
+                />
+              );
+            })
+          ) : (
             <VictoryLine
               style={{
                 data: { stroke: "#c43a31" },
                 parent: { border: "1px solid #ccc" }
               }}
               domain={{ y: [1, 10] }}
-              data={data.map((record, i) => {
-                lastDate = record.value;
+              data={records.map((record, i) => {
                 return {
                   y: record.value,
                   x: new Date(record.timestamp)
@@ -100,18 +145,9 @@ class InsightsScreen extends React.Component {
                 onLoad: { duration: 1000 }
               }}
             />
-          </VictoryChart>
-        ) : (
-          <View
-            height={350}
-            style={{ alignItems: "center", justifyContent: "center" }}
-          >
-            <Text>
-              Not enough data for {this.state.selectedTags.join(" + ")}
-            </Text>
-          </View>
-        )}
-        
+          )}
+        </VictoryChart>
+
         <View style={styles.tagContainer}>
           <Button
             style={styles.tag}
@@ -124,13 +160,20 @@ class InsightsScreen extends React.Component {
             onPress={this.handleSelectAll.bind(this)}
           />
           {tags.sort((a, b) => a.name.localeCompare(b.name)).map((tag, i) => {
+            if (!this.state.tree[tag.name]) {
+              return null;
+            }
+            if (this.state.tree[tag.name].records.length < 2) {
+              return null;
+            }
             const isSelected = this.state.selectedTags.indexOf(tag.name) > -1;
-            const color = COLORS[i % 10];
             return (
               <Button
                 style={styles.tag}
                 key={tag.id}
-                backgroundColor={isSelected ? color : "grey"}
+                backgroundColor={
+                  isSelected ? COLORS[this.state.selectedTags.indexOf(tag.name)] : "grey"
+                }
                 title={tag.name}
                 rounded
                 margin={0}
